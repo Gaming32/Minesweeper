2a076731a222fbebd3de0ed301d2c94d81c0bc03
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2020-07-07 17:06:12 -0500

    boilerplate pygame code

--- msgui.py
+++ msgui.py
@@ -6,6 +6,16 @@ def main():
     pygame.init()
     display = pygame.display.set_mode((640, 640))
     board = Minesweeper()
+    bomb_image = pygame.image.load('bomb.png').convert_alpha()
+    flag_image = pygame.image.load('flag.png').convert_alpha()
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        pygame.display.update()
 
 
 if __name__ == '__main__':
